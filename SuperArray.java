2050d0e39ef3997e8e96975fabb99fcb8a4e3f6e
xiaopies <xiaoshenma2016@gmail.com>
2020-11-03 01:55:22 -0500

    added size

--- SuperArray.java
+++ SuperArray.java
@@ -2,10 +2,18 @@ public class SuperArray{
   private String[] data;
   private int size;
 
-  public SuperArray(String[] data, int size){
+  public SuperArray( int size){
     this.data = data;
     this.size = size;
   }
+  public SuperArray(){
+    SuperArray superArray = new SuperArray(10);
+  }
 
+  public int size(){
+    return superArray.length;
+  }
   
+
+
 }
