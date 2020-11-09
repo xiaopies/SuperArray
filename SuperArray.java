6e2803e95dfb14a71be1f2b92fd60778672064a1
xiaopies <xiaoshenma2016@gmail.com>
2020-11-09 14:38:01 -0500

    tried error message for get

--- SuperArray.java
+++ SuperArray.java
@@ -25,6 +25,9 @@ public class SuperArray{
     return true;
   }
   public String get(int index){
+    if (index<0 || index>= size()){
+      throw new IndexOutOfBoundsException("index " + index + " is out of range");
+    }
     return data[index];
   }
   public String set(int index, String element){
--- Tester.java
+++ Tester.java
@@ -51,6 +51,8 @@ public class Tester{
       System.out.println("don't use negative");
     }
 
+    System.out.println(ords.get(14));
+
 
 
 
